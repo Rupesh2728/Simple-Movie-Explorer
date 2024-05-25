63712d23cec4cd7461a3c84d45bacecbe4047d5d
Rupesh_peddineni <rupeshprofessional2728@gmail.com>
2024-05-25 10:18:26 +0530

    Deployment done on Vercel and render

--- client/src/URL.js
+++ client/src/URL.js
@@ -1,5 +1,7 @@
-const BACKEND_URL="http://localhost:5000";
+// const BACKEND_URL="http://localhost:5000";
 const IMAGE_URL = "https://image.tmdb.org/t/p/w500/";
+const BACKEND_URL="https://simple-movie-explorer.onrender.com";
+
 module.exports ={
     BACKEND_URL,
     IMAGE_URL,
